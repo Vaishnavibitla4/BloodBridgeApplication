9a1454c3729b8f05da8a1bc94b590ac4844a26c6
Vaishnavi Bitla <vaishnavibitla4@gmail.com>
2025-08-24 11:24:47 +0530

    Starting on Backend

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+const dbConnection = require('./utils/db');
+dotenv.config();
+
+
+//port
+const PORT = process.env.PORT;
+
+
+//server
+app.listen(PORT, () => {
+    console.log(`Server is working on http://localhost:${PORT}`);
+    dbConnection();
+});
